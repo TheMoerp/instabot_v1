64ecff0814d475cfaad53f5c70483ecb6298e092
Matthias Hottgenroth <matthias.hottgenroth@icloud.com>
2021-02-27 18:50:05 +0100

    added log & conf code

--- igCreateAccount.py
+++ igCreateAccount.py
@@ -1,41 +1,47 @@
 import requests
 import json
+import time
+import logging
 from proxyCrawler import proxy_request
 from igPwdEncrypt import encrypt_password
 
-
+############################
 keyId = 212
 pubKey = "fd1b44ee842e66d2178f3b406a8468b7739cc14752a7c827fe77a46270437512"
-
-
-urlCmdPart = "/accounts/web_create_ajax/attempt/"
-
-token = "RHgkBu4tUfBVWMXFm1K456MnSaLZzurT"
+token = "MbpnYnqPmQZON1CzRcz2QXFfrbgsha2i"
 igAjax = "0edc1000e5e7"
+appId = "936619743392459"
+deviceId = "YDp6mgALAAE0Qu8WOeOqQlZ_xEP9" # new
+############################
+
+
+#### URL Path ####
+USERDATA_PATH = "/accounts/web_create_ajax/attempt/"
+AGE_PATH = "/web/consent/check_age_eligibility/"
+SEND_PATH = "/api/v1/accounts/send_verify_email/"
+CONF_PATH = "/api/v1/accounts/check_confirmation_code/"
+##################
+
+
 
 contentType = "application/x-www-form-urlencoded"
-baseUrl = "https://www.instagram.com"
-connectionType = "close"
+BASE_URL = "https://www.instagram.com"
 
-opt_into_one_tap = "false"
-
-mail = "EntzueckendUnfassbarerSkorpion@existiert.net"
+mail = "matthias@hottis.de"
 pwd = "daspasswordistsicher"
-username = "testnasdas"
-#first_name = "testman"
+username = "tesadstnasdas"
 
 
 
-#encPwd = encrypt_password(key_id, pub_key, pwd, version=10)
 
-#body = "email={}&enc_password={}&username={}&firstname={}&opt_into_one_tap=false".format(mail, encPwd, username, first_name)
+logging.basicConfig(filename='Logs/igLogon.log', level=logging.DEBUG)
 
 
-def BuildCredentialsBody(mail, keyId, pubKey):
+def BuildUserDataBody(mail, keyId, pubKey, username):
     pwd = "e1nzig@rt1gesP@5sw0rd"
     firstName = "IchBinKeinBot"
     encPwd = encrypt_password(keyId, pubKey, pwd, version=10)
-    body = "email={}&enc_password={}&username={}&firstname={}&opt_into_one_tap=false".format(mail, encPwd, username, firstName)
+    body = "email={}&enc_password={}&username={}&firstname={}&seamless_login_enabled=1&opt_into_one_tap=false".format(mail, encPwd, username, firstName)
 
     return body
 
@@ -48,24 +54,48 @@ def BuildAgeBody():
 
     return body
 
+def BuildSendMailBody(mail, deviceId):
+    body = "device_id={}&email={}".format(deviceId, mail)
 
-def PostRequest(baseUrl, urlCmdPart, connectionType, token, igAjax, contentType, body, workingProxy):
+    return body
+
+
+def BuildConfermationBody(mail, deviceId, confCode):
+    body = "code={}&device_id={}&email={}".format(confCode, deviceId, mail)
+
+    return body
+
+
+
+
+def PostRequest(baseUrl, urlCmdPart, token, igAjax, body, workingProxy):
     url = "{}{}".format(baseUrl, urlCmdPart)
     headers = {
-        "Connection": connectionType,
+        "Connection": "close",
         "X-Instagram-AJAX": igAjax,
-        "Content-Type": contentType,
+        "Content-Type": "application/x-www-form-urlencoded",
         "X-CSRFToken": token,
+        "Accept-Language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
+        "X-IG-App-ID": appId
     }
+
+    debugHeaders = "Connection: close\nContent-Type: application/x-www-form-urlencoded\nX-Instagram-AJAX: {}\nX-CSRFToken: {}\nX-IG-APP-ID: {}".format(igAjax, token, appId)
 
     respTuple = proxy_request('post', url, headers, body, workingProxy)
     resp = respTuple[0]
     proxy = respTuple[1]
-    print(resp.text)
+
+    logging.debug('\nURL: {}\nMethode: post\n\n--- Headers ---\n{}\n\n--- Body ---\n{}\n\n--- Response ---\n{}\n'.format(url, debugHeaders, body, resp.text))
 
     jsonObject = json.loads(resp.text)
     status = jsonObject["status"]
+
     return (status, proxy)
+
+
+
+
 
 
 def CreateAccountOnIG():
@@ -73,10 +103,9 @@ def CreateAccountOnIG():
     spamDedCnt = 0
 
     while status != "ok":
-        credBody = BuildCredentialsBody(mail, keyId, pubKey)
-        credUrlCmdPart = "/accounts/web_create_ajax/attempt/"
+        userDataBody = BuildUserDataBody(mail, keyId, pubKey, username)
 
-        statusTuple = PostRequest(baseUrl, credUrlCmdPart, connectionType, token, igAjax, contentType, credBody, '')
+        statusTuple = PostRequest(BASE_URL, USERDATA_PATH, token, igAjax, userDataBody, "")
         status = statusTuple[0]
         proxy = statusTuple[1]
 
@@ -89,12 +118,12 @@ def CreateAccountOnIG():
 
     print("--- the user data has been entered successfully ---")
     status = ''
-    
+    time.sleep(1.0)
+
     while status != "ok":
         ageBody = BuildAgeBody()
-        ageUrlCmdPart = "/web/consent/check_age_eligibility/"
 
-        statusTuple = PostRequest(baseUrl, ageUrlCmdPart, connectionType, token, igAjax, contentType, ageBody, proxy)
+        statusTuple = PostRequest(BASE_URL, AGE_PATH, token, igAjax, ageBody, proxy)
         status = statusTuple[0]
         proxy = statusTuple[1]
 
@@ -109,10 +138,27 @@ def CreateAccountOnIG():
     status = ''
 
     while status != "ok":
-        print("An confermation code has been send to the following mail-address:\n{}".format(mail))
+        print("An confermation code has been send to the following mail-address: {}".format(mail))
         confCode = input("Enter the confirmation code: ")
+        baseUrl = "https://i.instagram.com"
 
-            
+        confBody = BuildConfermationBody(mail, deviceId, confCode)
+        
+        statusTuple = PostRequest(baseUrl, CONF_PATH, token, igAjax, confBody, proxy)
+        status = statusTuple[0]
+        proxy = statusTuple[1]
+
+        if status == "fail":
+            spamDedCnt += 1
+            print("Instagram marked this request as spam. The Spam-detection is {}. Trying another proxy...\n".format(spamDedCnt))
+        elif status != "ok":
+            print("An unknown error has been received.")
+            exit()
+    
+    print("--- the confermation code has been accepted ---")
+    status = ''
+    time.sleep(1.0)
+
 
 
 
--- proxyCrawler.py
+++ proxyCrawler.py
@@ -3,15 +3,26 @@ from bs4 import BeautifulSoup
 from random import choice
 
 
+triedProxys = set()
+
 def get_proxy():
     url = 'https://www.sslproxies.org/'
     resp = requests.get(url)
     soup = BeautifulSoup(resp.content, 'html.parser')
 
+    # sameProxyCnt = 0
+    # while True:
     proxy = {'https': choice(list(map(lambda x:x[0]+':'+x[1], list(zip(map(lambda x:x.text, soup.findAll('td')[::8]),
                                     map(lambda x:x.text, soup.findAll('td')[1::8]))))))}
+        # if tuple(proxy) not in triedProxys:
+        #     break
+        # sameProxyCnt += 1
+        # print("Got the same Proxy for the {} time. Choosing another one...".format(sameProxyCnt))
+    
+    # triedProxys.add(tuple(proxy))
 
     return proxy
+
 
 def proxy_request(requerstType, url, headers, body, workingProxy):
     while True:
@@ -20,7 +31,6 @@ def proxy_request(requerstType, url, headers, body, workingProxy):
                 proxy = get_proxy()
             else:
                 proxy = workingProxy
-
             resp = requests.request(requerstType, url, headers=headers, data=body, proxies=proxy, timeout=5)
             print("\nRequest send with Proxy: {}".format(proxy))
             break
