70782d6ac2cc42a2d18e5cee013698b62462133a
moerp <matthias.hottgenroth@icloud.com>
2021-03-02 02:21:48 +0000

    Delete accountClass.py

--- accountClass.py
+++
@@ -1,9 +0,0 @@
-class Account(object):
-    def __init__(self, mail, name, username, password, day, month, year):
-        self.mail = mail
-        self.name = name
-        self.username = username
-        self.password = password
-        self.day = day
-        self.month = month
-        self.year = year
